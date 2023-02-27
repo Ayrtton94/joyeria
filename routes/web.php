304e2bbd5ca4da542666ea0d40d6cf1a32d624e1
Ayrtton94 <navarro.ayrtton@gmail.com>
2023-02-26 20:56:59 -0500

    linea2

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('linea', App\Http\Controllers\LineaController::class);
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('linea', App\Http\Controllers\LineaController::class);
+
+
+});
